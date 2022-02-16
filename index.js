657dfa13883956dcbb86ebe4452a2fdba18e1585
Landon Phillips <landonphillips@Landons-MBP.attlocal.net>
2022-02-16 17:04:54 -0600

    JS mvp finished

--- index.js
+++ index.js
@@ -39,12 +39,22 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = []
 }
-
-
-
+Person.prototype.eat = function(edible){
+  if (this.stomach.length < 10 ){
+    this.stomach.push(edible)
+  } 
+}
+Person.prototype.poop = function() {
+  this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return `${this.name} ${this.age}`
+}
 
 
 
@@ -63,8 +73,26 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon
+  this.tank = 0;
+  this.odometer = 0;
+  }
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons;
+}
+Car.prototype.drive = function(distance){
+  const driveMiles = this.tank * this.milesPerGallon;
+  if(distance <= driveMiles){
+    this.odometer = this.odometer + distance;
+    this.tank = this.tank - distance / this.milesPerGallon;
+  } else {
+    this.odometer = this.odometer + driveMiles;
+    this.tank = 0;
+    return `I ran out of fuel at ${this.odometer} miles`
+  }
+
 }
 
 
@@ -75,18 +103,25 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+ Person.call(this, name, age);
+ this.favoriteToy = favoriteToy;
+}
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function(){
+  return ` Playing with ${this.favoriteToy}`
 }
 
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. global Binding is when this is in the global scope. you get window object
+  2. implicit binding is when using a method, whatever is left to the . is bound.
+  3. explicit binding when we call a function to hook up 'this' and a given value 
+  4. new binding uses the keyword new 
 */
 
 
